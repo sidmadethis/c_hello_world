ef7e626477185a8ae69a3368645177fad985babb
sid <sidmadethis@gmail.com>
2017-12-12 18:30:30 -0700

    working on factorial

---
+++ factorial.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int factor;
+    printf("pick a number to factor: ");
+    scanf("%d", &factor);
+    printf("\nthis is the number you chose: %d\n", factor);
+}
+
+
