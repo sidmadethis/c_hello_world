c54cf73e19908ee6ef63e41f69ccf484e461f3e3
sid <sidmadethis@gmail.com>
2017-12-22 10:16:07 -0700

    back to c stuff

--- factorial.c
+++ factorial.c
@@ -5,6 +5,9 @@ int main(){
     printf("pick a number to factor: ");
     scanf("%d", &factor);
     printf("\nthis is the number you chose: %d\n", factor);
+
+    int total = factor * (factor - 1);
+    printf("\nnew number: %d\n", total);
 }
 
 
