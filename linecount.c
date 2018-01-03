5097ed811f67fe4b85996649829b6852a77c3b72
sid <sidmadethis@gmail.com>
2018-01-02 21:48:37 -0700

    learning more c

---
+++ linecount.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("I am alive! beware. \n");
+    getchar();
+    /*getchar - reads a single character and waits for user to hit enter before  reading the
+     * character.... to prevent window from closing before you see output */
+
+    return 0;
+}
+
