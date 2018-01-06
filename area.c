8a1afa695b7173700ab599f4eaf0f7c73060346d
sid <sidmadethis@gmail.com>
2018-01-06 09:41:12 -0700

    got doubles working. still learning

--- area.c
+++ area.c
@@ -2,14 +2,22 @@
 
 int main(){
 
-    float l,w=0.0;
+    double l,w=0.0;
     printf("Enter the length of a rectangle: ");
-    scanf("%f",l);
+    scanf("%lf",&l);
     printf("\nEnter the width of a rectangle: ");
-    scanf("%f",w);
+    scanf("%lf",&w);
     
+    /* the  & in the above scanf is needed to point to the correct 
+    memory address- without it the program tries to write in the
+    wrong sections of memory */
+
+    /* https://en.wikipedia.org/wiki/Scanf_format_string */
+
     float area = l * w;
-    printf("\nThe area of the rectangle is: %f", area);
+    printf("\nThe area of the rectangle is: %.2lf\n", area);
+    
+    /* the .2 after the % to manipulate the number of decimals? not sure */
 
     return 0;
 }
