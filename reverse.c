3a565448ab989692fd54c56d6efbe93d1a5a0517
sid <sidmadethis@gmail.com>
2018-01-13 11:24:30 -0700

    working on fgets

--- reverse.c
+++ reverse.c
@@ -10,8 +10,10 @@ int main() {
     printf("Enter a string to reverse: \n");
     //scanf is dangerous... buffer overflows.  maybe use fgets?
     //scanf("%s", forward_string);
-    fgets(forward_string, stdin);
+    fgets(forward_string, 20, stdin);
     //doesnt work need to learn how to use fgets
+    //fgets needs 3 arguments - the thing it is reading
+    // -some number, and some way to read it? still not positive
 
     printf("This is the string you entered: %s\n", forward_string);
     return 0;
