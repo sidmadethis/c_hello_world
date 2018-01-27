93abed0bcad2a1a0d355d8a9478aecc2bc34c236
sid <sidmadethis@gmail.com>
2018-01-27 10:56:59 -0700

    more work on reverse string

--- reverse.c
+++ reverse.c
@@ -11,7 +11,7 @@ int main() {
     printf("Enter a string to reverse: \n");
     //scanf is dangerous... buffer overflows.  maybe use fgets?
     //scanf("%s", forward_string);
-    fgets(forward_string, 20, stdin);
+    fgets(forward_string, 100, stdin);
     //doesnt work need to learn how to use fgets
     //fgets needs 3 arguments - the thing it is reading
     // -some number, and some way to read it? still not positive
@@ -20,7 +20,13 @@ int main() {
     
     //now to reverse the string. first thing to do is get the
     //total length of the string  ** need to include <string.h>
+    // i keep getting the return character in the lenght of the string, 
+    // would like to figure out a way to remove it without using -1 
 
+    //turns out i need to do something to forward_string because fgets adds the newline character
+
+    forward_string[strcspn( forward_string, "\n" )] = '\0';
+    //this seems to do it, but now i need to read up on strcspn...
     int k = strlen(forward_string);
     printf("this is the length of the string you entered: %d\n", k);
     
